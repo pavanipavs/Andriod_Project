54a79cc5608e95c3481752c36de63695602cd4e6
pavanipavs <brspavani@gmail.com>
2017-02-27 04:05:44 -0800

    Initial commit

---
+++ app/src/main/java/com/example/andriod/interviewquestions/FrontPage.java
@@ -0,0 +1,82 @@
+package com.example.andriod.interviewquestions;
+
+import java.net.URI;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+public class FrontPage extends AppCompatActivity implements OnClickListener {
+
+	Button bsimple, btough,  brateapp;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.front_page);
+
+		// Codes to add Action Bar
+
+		LinearLayout front_ll = (LinearLayout) findViewById(R.id.front_page_titlebar);
+		getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+		getSupportActionBar().setCustomView(R.layout.frontpage_title_bar);
+
+		bsimple = (Button) findViewById(R.id.bsq);
+		btough = (Button) findViewById(R.id.btq);
+
+		brateapp = (Button) findViewById(R.id.brateapp);
+
+		bsimple.setOnClickListener(this);
+		btough.setOnClickListener(this);
+
+		brateapp.setOnClickListener(this);
+
+	}
+
+	@Override
+	public void onClick(View v) {
+
+		switch (v.getId()) {
+		case R.id.bsq:
+
+			Intent i = new Intent(FrontPage.this, Simple_question.class);
+			startActivity(i);
+
+			break;
+		case R.id.btq:
+			
+			Intent j = new Intent(FrontPage.this, Tough_question.class);
+			startActivity(j);
+			
+			
+			break;
+
+		case R.id.brateapp:
+
+			try{
+				Uri uri1 = Uri.parse("market://details?id="+ getPackageName());
+				Intent goToMarket1 = new Intent(Intent.ACTION_VIEW, uri1);
+				startActivity(goToMarket1);
+			}catch(ActivityNotFoundException e){
+				
+				Uri uri1 = Uri.parse("http://play.google.com/store/apps/details?id="+ getPackageName());
+				Intent goToMarket1 = new Intent(Intent.ACTION_VIEW, uri1);
+				startActivity(goToMarket1);
+				
+			}
+			
+			
+			break;
+
+		}
+
+	}
+}
